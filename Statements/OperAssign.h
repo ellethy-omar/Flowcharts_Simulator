0af4154a66af2aa81522f0841e8170bd199c2f65
ellethy-omar <147427447+ellethy-omar@users.noreply.github.com>
2023-12-26 17:23:22 +0200

    Update OperAssign.h

--- Statements/OperAssign.h
+++ Statements/OperAssign.h
@@ -7,18 +7,29 @@ class OperAssign : public Statement
 private:
 	string LHS;
 	string RHS;
+
 	Connector* pOutConn;
+
 	Point Inlet;
 	Point Outlet;
 	Point LeftCorner;
+
 	virtual void UpdateStatementText();
+
 public:
 	OperAssign(Point Lcorner, string LeftHS = "", string RightHS = "");
-	void setLHS(const string& L);
-	void setRHS(string R);
-	Point getInlet();
-	Point getOutlet();
-	bool Is_In_Region(Point& p1);
-	virtual void Draw(Output* pOut) const;
-};
 
+	void setLHS(const string& L);
+
+	void setRHS(string R);
+
+	Point getInlet();
+
+	Point getOutlet();
+
+	bool Is_In_Region(Point& p1);
+
+	virtual void Draw(Output* pOut) const;
+
+	~OperAssign();
+};
