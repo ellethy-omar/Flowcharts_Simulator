c9b86ceaf0a9934591df67278ab106a9bdab261f
ellethy-omar <147427447+ellethy-omar@users.noreply.github.com>
2023-12-26 17:25:10 +0200

    Update End.h

--- Statements/End.h
+++ Statements/End.h
@@ -8,18 +8,7 @@
 class End : public Statement 
 {
 private:
-
-	//Value Assignment Stat. has one Connector to next statement
-	//Each statement type in flowchart has a predefined number of (output) connectors
-	//For example, conditional statement always has 2 output connectors
-
-	//Note: We don't need to keep track with input connectors
-	//      Whenever we want to iterate on all statements of the flowchart
-	//      we will begin with start statement then its output connector
-	//      then the connector's destination statement and so on (follow the connectors)
-
-	Point Inlet;	//A point where connections enters this statement 
-	//It's used as the (End) point of the (Input) connectors
+	Point Inlet;
 
 	Point Center;	//Center of the statement block.
 
@@ -29,7 +18,7 @@ public:
 
 	End(Point Center);
 
-	Point getOutlet();
+	Point getOutlet(int x = 0);
 
 	Point getInlet();
 
@@ -41,6 +30,7 @@ public:
 
 	void SetConnector(Connector* pOutConn);
 
+	Connector* GetConnector(int checker = 0);
 
 };
 #endif
