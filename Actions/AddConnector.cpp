dc7be93d58374efc4177766ad64aa146ae985f1a
ellethy-omar <147427447+ellethy-omar@users.noreply.github.com>
2023-12-26 22:44:46 +0200

    Update AddConnector.cpp

--- Actions/AddConnector.cpp
+++ Actions/AddConnector.cpp
@@ -2,55 +2,94 @@
 #include "..\ApplicationManager.h"
 #include "..\GUI/Input.h"
 #include "..\GUI/Output.h"
+#include "..\Statements\End.h"
+#include "..\Statements\Start.h"
+#include "..\Statements\Condition.h"
+#include "Delete.h"
 
 AddConnector::AddConnector(ApplicationManager* pAppManager) :Action(pAppManager)
-{}
+{
+
+	s1 = NULL;
+	s2 = NULL;
+
+}
 
 void AddConnector::ReadActionParameters()
-{}
-
-Statement* AddConnector::ReadActionParametersConnectors(Statement* & s1)
 {
 	Input* pIn = pManager->GetInput();
 	Output* pOut = pManager->GetOutput();
 
-	pOut->PrintMessage("Connector : Click on the first delivering statment");
+	Point Position;
 
+	pOut->PrintMessage("Connector : Click on the first delivering statment");
 	pIn->GetPointClicked(Position);
 
-	while (pManager->GetStatement(Position) == NULL)
-	{
-		pOut->PrintMessage("Please, select on the delivering statment");
-	}
 	s1 = pManager->GetStatement(Position);
 
-	Statement* s2 = NULL;
+	Statement* test;
+	test = dynamic_cast<End*>(s1);
 
+	while (pManager->GetStatement(Position) == NULL || test != NULL)
+	{
+		pOut->PrintMessage("Please, select on the delivering statment such that it isn't a start statement");
+		pIn->GetPointClicked(Position);
+		s1 = pManager->GetStatement(Position);
+		test = dynamic_cast<End*>(s1);
+	}
+
+	s1 = pManager->GetStatement(Position);
+	s1->GetConnector();
+	x = 0;
+	test = dynamic_cast<Condition*>(s1);
+
+	if (test != NULL)
+	{
+		pOut->PrintMessage("Selected a Condion statement  Enter 1 for true, Enter 2 for false, then click on the conditonal again to continue");
+		pIn->GetPointClicked(Position);
+		x = int(pIn->GetValue(pOut));
+		while (pManager->GetStatement(Position) != s1)
+		{
+			pOut->PrintMessage("Click on the same conditional to confirm");
+			pIn->GetPointClicked(Position);
+		}
+		while ((x != 1) && (x != 2))
+		{
+			x = int(pIn->GetValue(pOut));
+		}
+	}
 	pOut->PrintMessage("Successfully made delivering statment, now click on recieveing statment");
 	pIn->GetPointClicked(Position);
 
-	while (pManager->GetStatement(Position)->Is_In_Region(Position) == false)
+	s2 = pManager->GetStatement(Position);
+	test = dynamic_cast<Start*>(s2);
+
+
+	while ((pManager->GetStatement(Position) == NULL || pManager->GetStatement(Position) == s1) || test != NULL)
 	{
 		pOut->PrintMessage("Please, select on the recieveing statment");
+		pIn->GetPointClicked(Position);
+		s2 = pManager->GetStatement(Position);
+		test = dynamic_cast<Start*>(s2);
 	}
-	s2 = pManager->GetStatement(Position);
-	return s2;
 }
 
 void AddConnector::Execute()
 {
-	Statement* stat1 = NULL;
-	Statement* stat2 = ReadActionParametersConnectors(stat1);
+	ReadActionParameters();
+	Point dummy;
+	Connector* pCond = new Connector(s1, s2, x);
 
-	int x = pManager->GetStatCount();
-	ForbiddenReigon* ForbiddenArr = new ForbiddenReigon[x];
-
-	for (int i = 0; i < x; i++)
+	for (int i = 0; i < pManager->GetStatCount(); i++)
 	{
-		ForbiddenArr[i] = pManager->GetStatementIteration(i)->GetForbiddenReigon();
+		if (pManager->GetStatementIteration(i) != s1 || pManager->GetStatementIteration(i) != s2)
+		{
+			if (pCond->Is_In_Region(pManager->GetStatementIteration(i)) == true)
+			{
+				
+			}
+		}
 	}
-	Connector* pCond = new Connector(stat1, stat2, ForbiddenArr , x);
-	
-
-}
+	pManager->AddConn(pCond);
+}	
 
